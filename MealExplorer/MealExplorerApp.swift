a62e2c7f043cca34053c4bf8ad2763e223821afe
amulyaAmmu <40600002+amulyaAmmu@users.noreply.github.com>
2024-04-26 15:08:57 -0700

    Initial Commit

---
+++ MealExplorer/MealExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealExplorerApp.swift
+//  MealExplorer
+//
+//  Created by Amulya Gangam on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct MealExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
